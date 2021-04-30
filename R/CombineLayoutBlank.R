457039250a3160e2d89302053765a33dc4b0e7f5
ddebrauske <ddebrauske@gmail.com>
2021-04-30 14:15:27 -0500

    polished annotate script. updated combinelayoutblanks to return plate vector as character

--- R/CombineLayoutBlank.R
+++ R/CombineLayoutBlank.R
@@ -9,11 +9,11 @@ CombineLayoutBlank <- function(layoutDF, blankDF){
 
 
       #First we'll do a checkpoint to make sure things are lining up
-      
-    
+
+
       layoutDF$Plate <- factor(layoutDF$Plate)
       blankDF$Plate <- factor(blankDF$Plate)
-    
+
     print("checking to see if layout and blank DFs match")
       if(nrow(layoutDF) == nrow(blankDF)){ #check to see if layout and blank match lengths
         print(":-) Blank and layout match lengths")
@@ -26,32 +26,32 @@ CombineLayoutBlank <- function(layoutDF, blankDF){
         stop(" :-( number of plates do not match!")
       }
       print(":-) Checkpoint passed")
-    
+
     layout.blanks <- matrix(ncol=1, nrow=nrow(layoutDF)) ## this new dataframe will be the combination of condition, strain, coordinate, Biological rep and plate#. it will combine the two dataframes generated above by matching up coordinate and plate# columns row by row and add the respective OD600 value to df.
-    
+
     layout.blanks <- as.data.frame(layout.blanks)
     layout.blanks <- cbind(layout.blanks, layoutDF)
     names(layout.blanks)[1] <- "Blank_OD600"
-    
+
     layout.blanks <- layout.blanks[,c(-3,-4)] #Removes X and Y matrix value from well nformation, because this now exicsts as a combined "coordimate" A1, for example.
-    
+
     print("this may take some time, please be patient")
-    
+
     for(i in 1:nrow(layout.blanks)){ #step through layout blanks row by row
       for(j in 1:nrow(blankDF)){ #step through blank.out until you find a matching coordinate&&plate#
         layout.coord <- layoutDF$Coordinate[i]
         blank.coord <- blankDF$Coordinate[j]
         layout.plate.number <- as.character(layout.blanks$Plate[i])
         blank.plate.number <- as.character(blankDF$Plate[j])
-    
-    
+
+
         if(layout.coord == blank.coord && layout.plate.number == blank.plate.number){ #if they match, add OD600 value to the new column of row i
           layout.blanks$Blank_OD600[i] <- as.numeric(as.character(blankDF$OD600[j]))
         }
       }
     }
 
-  
+  layout.blanks$Plate <- as.character(layout.blanks$Plate) #needed to return to character vector for Annotation script
   return(layout.blanks)
-  
+
   }
